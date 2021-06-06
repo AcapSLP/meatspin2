ca105ea514666f2b4d76886246d4deaead473510
AcapSLP <72445644+AcapSLP@users.noreply.github.com>
2021-06-06 18:03:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showDiv() {
+    div = document.getElementById('div1');
+    div.style.display = "block";
+    
+}
+var vAudio = document.getElementById("divAudio");
+            function playMusic()
+            {
+               vAudio.play();
+            }
+function hide() {
+            var button = document.getElementById('showbutton');
+            button.style.display = 'none';
+            }
